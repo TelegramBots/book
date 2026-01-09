373359eb41a4c41b0739f0bd94781b79b2e3bd6c
wiz0u <wiz0u@users.noreply.github.com>
2026-01-09 16:13:21 +0000

    deploy: 11c0b7ca17b08e2cb07758fb9c8dacceb5782dcc

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-813e57c6.js',
+                path_to_root + 'searchindex-bb818959.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
